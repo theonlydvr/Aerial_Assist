511a5c3496da526e7ef7be149d745f3869160558
theonlydvr <theonlydvr@gmail.com>
2015-09-15 22:45:02 +0200

    Update RobotTemplate.java

--- RobotA/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ RobotA/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -16,36 +16,59 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.templates.BadAssController.AxisType;
 import edu.wpi.first.wpilibj.templates.BadAssController.ButtonType;
 
+/*
+ * This is the main class for the robot from 2014. It has code to interact with all the sensors and motors on the robot and some methods for automation
+ */
 
 public class RobotTemplate extends SimpleRobot {
+    /*
+     * Class for interacting with the tilting platform
+     */
     class Platform extends Talon 
     {
-        AnalogPotentiometer pot;
+        AnalogPotentiometer pot; // the potentiometer which provided an angle reading to adjust the platofrm
+        
+        /*
+         * Takes the channel on the electronics board and a constructed potentiometer (which is done using a different channel) to create a Platform object
+         */
         public Platform(int channel, AnalogPotentiometer pot) {
             super(channel);
             this.pot = pot;
         }
         
+        
+        /*
+         * Asjusts the angle of the Platform until it is at the desired value
+         */
         public void setAngle(double angle)
         {
-            set(angle < pot.get() ? -0.4 : 0.4);
+            set(angle < pot.get() ? -0.4 : 0.4); // fancy values that were found by testing
             long startTime = System.currentTimeMillis();
             while (Math.abs(angle - Math.abs(pot.get())) > 0 && System.currentTimeMillis() - startTime < 3000){}
             set(0);
         }
         
+        /*
+         * Gets the angle from the potentiometer, pretty simple
+         */
         public double getAngle()
         {
             return pot.get();
         }
     }
     
+    /*
+     * Class used to interact with the shooting mechanism
+     */
     class Shooter extends Talon
     {
         public Shooter(int channel) {
             super(channel);
         }
         
+        /*
+         * Pulled the shooter back until the mechanism hit the limit switch signaling that more pulling would cause it to fire
+         */
         public void load()
         {
             shooter.set(0.5);
@@ -56,6 +79,10 @@ public class RobotTemplate extends SimpleRobot {
             Timer.delay(2);
             shooter.set(0);
         }
+        
+        /*
+         * After being loaded would pull the mechanism the last little bit in order to fire
+         */
         public void shoot()
         {
             //double angle = aim();
@@ -67,6 +94,10 @@ public class RobotTemplate extends SimpleRobot {
             shooter.set(0);
         }
         
+        
+        /*
+         * Fancy function meant to be used during autonomous to calculate how the robot should aim and fire the ball, was never fully tested or used
+         */
         public void aim()
         {    
             double Xt = ultrasonic.getDistance();
@@ -190,6 +221,9 @@ public class RobotTemplate extends SimpleRobot {
         }
     }
     
+    /*
+     * A class used to interact with an Ultrasonic sensor and get values from it
+     */
     class Ultrasonic extends AnalogChannel
     {
         double distance;
@@ -216,10 +250,10 @@ public class RobotTemplate extends SimpleRobot {
     Ultrasonic ultrasonic = new Ultrasonic(2);
     Platform platform = new Platform(6, platformAngle);
     Shooter shooter = new Shooter(5);
-    DigitalInput stop90 = new DigitalInput(1);
-    DigitalInput stop20 = new DigitalInput(10);
-    DigitalInput stopLoad = new DigitalInput(9);
-    Servo leftSpeed = new Servo(7);
+    DigitalInput stop90 = new DigitalInput(1); // arms at right angle
+    DigitalInput stop20 = new DigitalInput(10); // arm at 20 degree angle
+    DigitalInput stopLoad = new DigitalInput(9); // limit switch to signal that the shooting mechanism is loaded
+    Servo leftSpeed = new Servo(7); 
     Servo rightSpeed = new Servo(9);
     
     public void robotInit()
@@ -227,6 +261,9 @@ public class RobotTemplate extends SimpleRobot {
         
     }
     
+    /*
+     * Drives forward... NASA worthy autonomous
+     */
     public void autonomous() {
         chassis.setSafetyEnabled(false);
         Timer.delay(5);
@@ -252,13 +289,14 @@ public class RobotTemplate extends SimpleRobot {
      * This function is called once each time the robot enters operator control.
      */
     public void operatorControl() {
-        System.out.println("hey");
+        System.out.println("hey"); // log some stuff
         chassis.setSafetyEnabled(false); 
-        boolean loaded = false, releasing = false, prepPick = false, finishPick = false;
+        boolean loaded = false, releasing = false, prepPick = false, finishPick = false; // flags to signal automated functions
         long startTime = 0;
         
         while (isOperatorControl() && isEnabled())
         {
+            // automatically loads the shooting mechanism if it is not releasing or already loaded
             if (controlStick.getStart() || (!loaded && !releasing))
             {
                 shooter.set(-1);
@@ -270,6 +308,7 @@ public class RobotTemplate extends SimpleRobot {
             
             loaded = stopLoad.get();
             
+            // gear change
             if (leftStick.getRawButton(2) || rightStick.getRawButton(2))
             {
                 if (leftSpeed.get() > 0.5 && rightSpeed.get() > 0.5)
@@ -285,23 +324,29 @@ public class RobotTemplate extends SimpleRobot {
                 Timer.delay(0.5);
             }
             
+            // drive controls, nice and simple
             chassis.tankDrive(-leftStick.getY(), -rightStick.getY());
             
+            // move rollers forward
             if (controlStick.getButton(ButtonType.kRightStick))
             {
                 rollers.set(Value.kForward);
             }
+            // move rollers backwards
             else if (controlStick.getButton(ButtonType.kLeftStick))
             {
                 rollers.set(Value.kReverse);
             }
             
+            // if the arms are being put into a position in order to pick a ball up
             if (prepPick)
             {
+                // stop the arms when they reach 20 degrees
                 if (stop20.get())
                 {
                     arms.set(0);
                 }
+                // in case the switch is not working stop after 3 seconds
                 if (System.currentTimeMillis() - startTime > 3000)
                 {
                     platform.set(0);
@@ -311,12 +356,15 @@ public class RobotTemplate extends SimpleRobot {
                 }
             }
             
+            // if the ball has been picked up and it has been signaled thart it should be brought in
             if (finishPick)
             {
+                // stop the arms when they reach 90 degrees
                 if (stop90.get())
                 {
                     arms.set(0);
                 }
+                // in case the switch is not working stop after 3 seconds
                 if (System.currentTimeMillis() - startTime > 3000)
                 {
                     arms.set(0);
@@ -327,6 +375,7 @@ public class RobotTemplate extends SimpleRobot {
                 }
             }
             
+            // button input to prepare ball pick up, singlas to the dashboard to graphically display a clicked button
             if (controlStick.getAButton())
             {
                 SmartDashboard.putBoolean("A", true);
@@ -336,6 +385,7 @@ public class RobotTemplate extends SimpleRobot {
             }
             else SmartDashboard.putBoolean("A", false);
             
+            // button input to finish ball pick up, singlas to the dashboard to graphically display a clicked button
             if (controlStick.getBButton())
             {
                 SmartDashboard.putBoolean("B", true);
@@ -345,6 +395,7 @@ public class RobotTemplate extends SimpleRobot {
             }
             else SmartDashboard.putBoolean("B", false);
             
+            // just gives response that button is clicked
             if (controlStick.getXButton())
             {
                 //finishPick();
@@ -353,6 +404,7 @@ public class RobotTemplate extends SimpleRobot {
             }
             else SmartDashboard.putBoolean("X", false);
             
+            // turns the rollers off
             if (controlStick.getYButton())
             {
                 //finishPick();
@@ -362,12 +414,14 @@ public class RobotTemplate extends SimpleRobot {
             }
             else SmartDashboard.putBoolean("Y", false);
             
+            // manual shooting
             if (controlStick.getTrigger(GenericHID.Hand.kRight) && loaded)
             {
                 shooter.set(-1);
                 Timer.delay(1);
             }
             
+            // release the shooting mechanism
             if (controlStick.getTrigger(GenericHID.Hand.kLeft))
             {
                 shooter.set(1);
@@ -379,8 +433,10 @@ public class RobotTemplate extends SimpleRobot {
                 shooter.set(0);
             }
             
+            // move the motors using input from the controller
             platform.set(controlStick.getAxis(AxisType.kLeftY));
             arms.set(-controlStick.getAxis(AxisType.kRightY) / 1.25);
+            // log data to the dashboard
             SmartDashboard.putNumber("Ultrasonic: ", ultrasonic.getDistance() * 2.54);
             SmartDashboard.putNumber("Potentiometer: ", platformAngle.get());
             SmartDashboard.putNumber("Left Stick", leftStick.getY());
@@ -404,6 +460,9 @@ public class RobotTemplate extends SimpleRobot {
     
     }
     
+    /*
+     * A method used to automate the process for moving the arms down and making the rollers move in order to pick up the ball
+     */
     public void prepPick()
     {
         arms.set(0.5);
@@ -412,12 +471,18 @@ public class RobotTemplate extends SimpleRobot {
         
     }
     
+    /*
+     * Brings the ball onto the platform once it has been picked up
+     */
     public void finishPick()
     {
         arms.set(-0.5);
         platform.set(0.5);
     }
     
+    /*
+     * Reverses the rollers and lowers the arms in order to spit the ball out, then brings the arms back to standard position
+     */
     public void release()
     {
         rollers.set(Value.kReverse);
